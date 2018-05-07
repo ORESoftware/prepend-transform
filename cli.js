97b581e222a34b32f1c5f13c9f3b83175b0f237e
Olegzandr VD <alex@oresoftware.com>
2018-05-07 00:18:27 -0700

    0.0.1013

--- cli.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-var _1 = require(".");
-var v = process.argv[2] || '';
-process.stdin.resume().pipe(_1.pt(v)).pipe(process.stdout);
--- index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import * as stream from 'stream';
-export interface IPTOptions {
-    omitWhitespace: boolean;
-}
-export declare const pt: (pre: string, $options?: Partial<IPTOptions>) => stream.Transform;
-export default pt;
